80d464085f2eaf624fd9ddd110298fcb8f4b76dd
Nishant3105 <nishantkumar2522@gmail.com>
2024-05-30 21:54:53 +0530

    Signup and Authentication

---
+++ Authentication-main/src/components/Auth/AuthForm.js
@@ -0,0 +1,86 @@
+import { useState, useRef } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false)
+
+  const emailReference=useRef()
+  const passwordReference=useRef()
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+    // setIsLoading((prevState) => !prevState);
+  };
+
+  const handleLoading=(e)=>{
+    e.preventDefault()
+    setIsLoading((prevState) => !prevState);
+  }
+  
+  const signupHandler =async ()=>{
+    setIsLoading((prevState) => !prevState);
+    try{
+      const enteredEmail=emailReference.current.value
+      const enteredPassword=passwordReference.current.value
+      const userCreds={
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }
+      const response=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDk6BaMAPvSu4Boffm_G3IV3dU7lcV03HA',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userCreds)
+      })
+      
+      if(!response.ok){
+        throw new Error('SOmething went wrong')
+      }
+      if(response.ok){
+        setIsLoading((prevState) => !prevState);
+      }
+
+    }catch(error){
+      alert('please enter correct creds')
+    }
+  }
+  
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form >
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' ref={emailReference} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            ref={passwordReference}
+            required
+          />
+        </div>
+        <div className={classes.actions}>
+          {isLogin ? (isLoading ? <p className={classes.p}>Sending Request...</p> : <button onClick={handleLoading}>Login</button>) : <button onClick={signupHandler} >Sign Up</button>}
+          {/* {isLoading && <p className={classes.p}>Sending Request...</p> } */}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
